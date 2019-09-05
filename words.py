e07b09c3243f0303d49f89012660cf5f373fa391
Illia Romanov <illia.romanov@illia.romanov.MacBook>
2019-09-05 13:58:56 +0300

    words.py

--- words.py
+++ words.py
@@ -4,8 +4,11 @@
 def reverse_words(text):
    
     result = []
+     # разбить по пробелам .split() thank you SO
     for word in text.split(" "):
-        result.append(word[::-1])      
+    	# развернуть каждое слово word[::-1]
+        result.append(word[::-1])  
+        # склеить в строку ' '.join(["vanya", "petya"])    
     return print(' '.join(result)) 
 
 reverse_words(text=input("print string\n"))
